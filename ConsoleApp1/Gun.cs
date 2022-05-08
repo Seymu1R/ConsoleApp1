71720a50e44e5ecddfd12bebea9f1e4ae14978d5
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-05-08 23:34:36 +0400

    Add project files.

---
+++ ConsoleApp1/Gun.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Gun
+    {
+        public string Typee;
+        public int TotalBulletSize;
+        public  int MagazineSize;
+        public int BylletLeft=0;
+        public int Reloader=0;
+        
+        
+        public Gun(string Type,int totalbulletsize,int magazinesize)
+        {
+            Typee = Type;
+            TotalBulletSize = totalbulletsize;
+            MagazineSize = magazinesize;
+            
+        }
+        public void shoot() {
+            if (BylletLeft >= 0 && MagazineSize >= 0)
+            {
+                
+                if (BylletLeft <= 10 && BylletLeft>=0 && MagazineSize>0)
+                {
+                    BylletLeft = --MagazineSize;
+                    if (Reloader<10)
+                    {
+                        ++Reloader;
+                    }                    
+                    Console.WriteLine("Ates olundu ve  " + BylletLeft + " eded gulle qaldi");
+                }
+                else
+                {
+                    Console.WriteLine(" ates olunmadi magazinde gulle yoxdur ve  Zehmet olmasa reload edin");
+                }
+            }          
+                  
+            
+        }
+        public void reload()
+        {
+            TotalBulletSize = TotalBulletSize - Reloader;
+            MagazineSize = BylletLeft + Reloader;
+            Reloader = 0;
+            Console.WriteLine("Reload olundu ve total  " + TotalBulletSize + "  eded gullemiz qaldi");
+            Console.WriteLine("Reload olundu ve magazin makslandi magazindeki gulle sayi " + MagazineSize);
+        }
+        public void FullShoot()
+        {
+            BylletLeft = MagazineSize-MagazineSize;
+            Reloader = MagazineSize;
+            Console.WriteLine("Ates olundu ve  " + BylletLeft + " eded gulle qaldi");
+        }
+        public void GetInformation() {
+            Console.WriteLine("Silahin adi-  " + Typee +"\nTotal Gulle tutumu-  " + TotalBulletSize + "\nMagazinde olan gulle sayi-  "+MagazineSize );
+        }
+        
+        
+
+    }
+    
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1- Shoot \n 2 - Reload Gun \n 3- Full Shoot \n 4-Gun information \n 5-exit");
+            int a = int.Parse(Console.ReadLine());            
+            Gun gun1 = new Gun("Glock-18", 100, 10);            
+            do
+            {
+                if (a==1)
+                {
+                    gun1.shoot();                    
+                }
+                else if (a==2)
+                {
+                    gun1.reload();
+                }
+                else if (a==3)
+                {
+                    gun1.FullShoot();
+                }
+                else if (a==4)
+                {
+                    gun1.GetInformation();
+                }
+                else if (a==5)
+                {
+                    Environment.Exit(a);
+                }
+                Console.WriteLine("zehmet olmasa ekrandaki reqemleri daxiledin");
+                Console.WriteLine("1- Shoot \n 2 - Reload Gun \n 3- Full Shoot \n 4-Gun information \n 5-exit");
+                a = int.Parse(Console.ReadLine());
+            } while (a != 5);
+
+
+
+
+        }
+    }
+}
